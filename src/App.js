b8d702ceb7ad57195880f2ede163cfc04289e22c
Nikita Mehata <nikitamehata23@gmail.com>
2019-08-07 09:45:53 -0500

    resolved merge conflicts

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          Presidential Race.
+        </h1>
+        <h3>
+          Coming Soon.
+        </h3>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
