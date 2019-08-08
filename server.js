855cc183ebed1b8a99cbb76db18760c1d7c62050
Nikita Mehata <nikitamehata23@gmail.com>
2019-08-08 09:43:14 -0500

    set the server.js

--- server.js
+++ server.js
@@ -1,31 +1,61 @@
+/* eslint-disable no-undef */
+//require("dotenv").config();
 const express = require("express");
-const mongoose = require("mongoose");
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+mongoose.set(useNewUrlParser, true);
+// var passport = require("passport");
+// var session = require("express-session");
 const bodyParser = require("body-parser");
-const passport = require("passport");
-const users = require("./routes/api/users");
+
 const app = express();
-// Bodyparser middleware
-app.use(
-  bodyParser.urlencoded({
-    extended: false
-  })
-);
+const PORT = process.env.PORT || 3001;
+
+// Middleware
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static("public"));
+
+//For BodyParser
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-// DB Config
-const db = require("./config/keys").mongoURI;
-// Connect to MongoDB
-mongoose
-  .connect(
-    db,
-    { useNewUrlParser: true }
-  )
-  .then(() => console.log("MongoDB successfully connected"))
-  .catch(err => console.log(err));
-// Passport middleware
-app.use(passport.initialize());
-// Passport config
-require("./config/passport")(passport);
+
+// // For Passport
+// app.use(session({ secret: "keyboard cat",resave: true, saveUninitialized:true})); // session secret
+ 
+// app.use(passport.initialize());
+ 
+// app.use(passport.session()); // persistent login sessions
+
+
+
 // Routes
-app.use("/api/users", users);
-const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server up and running on port ${port} !`));
+app.use('/users', require('./apiauthentication/routes/users'));
+
+
+var syncOptions = { force: false };
+
+// If running a test, set syncOptions.force to true
+// clearing the `testdb`
+if (process.env.NODE_ENV === "test") {
+  syncOptions.force = true;
+}
+//Connect to Mongoose:
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/esk");
+
+// Starting the server, syncing our models ------------------------------------/
+// db.sequelize.sync(syncOptions).then(function () {
+  app.listen(PORT, function (err) {
+    if (!err) {
+      console.log(
+        "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+        PORT,
+        PORT
+      );
+    }
+    else{console.log(err, "Something went wrong with the possport db update");}
+  });
+// });
+
+module.exports = app;
