154de0f113421baf935c840f003ae57bf8075238
Daniel Williams <daniellanierwilliams@gmail.com>
2019-08-13 13:47:18 -0500

    set to when charatcer is selected it displays the kart and course that will be played

--- client/src/components/Racer/index.js
+++ client/src/components/Racer/index.js
@@ -2,6 +2,12 @@ import React from "react";
 import "./style.css";
 import Elizabeth from "./images/elizabeth.jpg";
 import Trump from "./images/trump.jpg";
+import Bluecar from "./images/bluecar.jpg";
+import Redcar from "./images/redcar.jpg";
+import Ovaloffice from "./images/ovaloffice.jpg";
+
+
+
 import Nav from "../Nav";
 import Wstart from "../wstart";
 import Tstart from "../tstart";
@@ -11,13 +17,18 @@ function Racer(props) {
         if(props.character==="elizabeth"){
             return (
             <div>
+            <img src={Bluecar} id="elizabeth"/>
              <img src={Elizabeth} id="elizabeth"/>
+             <img src={Ovaloffice} id="elizabeth"/>
+
             <Wstart />
             </div>)
         }else if( props.character==="trump")        {
             return (
             <div>
+            <img src={Redcar} id="trump"/>
             <img src={Trump} id="trump"/>
+            <img src={Ovaloffice} id="trump"/>
            <Tstart />
             </div> )
         }
@@ -26,7 +37,7 @@ function Racer(props) {
         <div>
             <Nav />
             <div className="container-2">
-                <p className="select-title">Select Racer</p>
+                <p className="select-title">Click to change racer</p>
                 <div className="images" onClick={() => props.changeCharacter("trump")}>
                     {whichCharacter()}
                 </div>
